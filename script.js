04fbd70255ef31f462fc1b60df96c6dd61d5bec0
Ayush Maheshwari <111050424+FrostScythe@users.noreply.github.com>
2025-09-20 21:55:14 +0530

    Refactor employee management functions in script.js

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-/** @format */
-
-let arr = [
+let employees = [
   { id: 1, name: "john", age: "18", profession: "developer" },
   { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
+  { id: 3, name: "karen", age: "19", profession: "admin" }
 ];
 
+// 1. Print developers using .map
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  employees.map((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
+// 2. Print developers using .forEach
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  employees.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
+// 3. Add new employee data
 function addData() {
-  //Write your code here, just console.log
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  employees.push(newEmployee);
+  console.log(employees);
 }
 
+// 4. Remove employee with profession admin
 function removeAdmin() {
-  //Write your code here, just console.log
+  employees = employees.filter((employee) => employee.profession !== "admin");
+  console.log(employees);
 }
 
-function concatenateArray() {
-  //Write your code here, just console.log
+// 5. Concatenate with another array
+function ConcatinateArray() {
+  let newArray = [
+    { id: 5, name: "alex", age: "22", profession: "manager" },
+    { id: 6, name: "emma", age: "23", profession: "designer" },
+    { id: 7, name: "mike", age: "21", profession: "tester" }
+  ];
+
+  let combinedArray = employees.concat(newArray);
+  console.log(combinedArray);
 }
